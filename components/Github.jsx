7bae700b86e0dc6b946e8fe99b56e51b2f713f17
Ndeta <ndetainnocent3@gmail.com>
2023-07-09 16:43:18 +0300

    Adding a div section to hold content from api entpoints and converting date to a readeable format in Portfolio.jsx

--- components/Github.jsx
+++ components/Github.jsx
@@ -3,9 +3,9 @@ import React from 'react'
  const  GitHub=async()=> {
     //Fetching data from gethub
      const response=await fetch('https://api.github.com/users/Ndeta100')
-
      if(!response.ok) throw new Error('Failed to fetch new data')
     const data=await response.json()
+    //Destructing fields from api fetch
     const { login, id,company, avatar_url, html_url, name, blog,bio,twitter_username,followers, following, created_at, updated_at, public_repos } = data;
     //Formating dates in Javascript
     let create_at_format = created_at.toString().slice(0, 10);
@@ -18,26 +18,23 @@ import React from 'react'
     <div className='space-x-4'>
     <img className='w-13 h-12 rounded-full ring-2 ring-gray-300 dark:ring-gray-500 mt-8' src={avatar_url} alt="Profile Avatar" />
     </div>
-    <div>
-    <p className='mt-4'>Hello!, my name is <span>{name}</span> I am a junior software developer at <span>{company}</span>, passionate about Software Development and Entrepreneurship. I enjoy working with server side code (Java, Golang, Node.js, Rust), and currently learning frontend (Next.js) at my new job, also a fan of SQL and NoSQL databse. Beside work I love playing football and learning new things </p>
+    <div className='  mt-6 border rounded-lg shadow'>
+    <p className='mt-4 mx-4 my-4'>Hello!, my name is <span>{name}</span> I am a junior software developer at <span>{company}</span>, passionate about Software Development and Entrepreneurship. I enjoy working with server side code (Java, Golang, Node.js, Rust), and currently learning frontend (Next.js) at my new job, also a fan of SQL and NoSQL databse. Beside work I love playing football and learning new things </p>
     </div>
     
     <a href={html_url} target='_blank' className='mt-4'>GitHub Profile details </a>
-    <div className='grid grid-cols-2 gap-4 mt-4'>
-    <p>GitHub: {login}</p>
-    <p>GitHub Id : {id}</p>
-    <p>blog: {blog}</p>
-    <p>GitHub Bio: {bio}</p>
-    <p>Twitter username: {twitter_username}</p>
-    <p>Followers: {followers}</p>
-    <p>Following: {following}</p>
-    <p>Public repos: {public_repos}</p>
-    <p>Created At: {create_at_format}</p>
-    <p>Updated At: {updated_at_format}</p>
+    <div className='grid grid-cols-2 gap-2 mt-4 border rounded-lg shadow '>
+    <p className='mx-6 my-4'>GitHub: {login}</p>
+    <p className='my-4 mr-4'>GitHub Bio: {bio}</p>
+    <p className='mx-6 my-4'>Twitter username: {twitter_username}</p>
+    <p className='my-4'>GitHub Followers: {followers}</p>
+    <p className='mx-6 my-4'> GitHub Followings: {following}</p>
+    <p className='my-4'>Public repos: {public_repos}</p>
+    <p className='mx-6 my-4'>Created At: {create_at_format}</p>
+    <p className=' my-4'>Updated At: {updated_at_format}</p>
     </div>
   </div>
    )
 }
-
    export default GitHub
 
--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -6,6 +6,20 @@ const Portfolio=async()=>{
      if(!response.ok) throw new Error('Failed to fetch new data')
     const data=await response.json()
     console.log(data[0].id);
+    return(
+        <div>
+      <h1 className='flex flex-col items-center mt-11'>My Portfolio</h1>
+      {data.map((item) => (
+        <div className="grid grid-cols-2 gap-2 mt-4 border rounded-lg shadow" key={item.id}>
+          <h2 className='my-4 mr-4'>Project name: {item.name}</h2>
+          <p className='my-4 mr-4'>Project description: {item.description}</p>
+          <p className='my-4 mr-4'>Programming Langusge used: {item.language}</p>
+          <p className='my-4 mr-4'>Creation date: {item.created_at.toString().slice(0,10)}</p>
+          <p className='my-4 mr-4'>Last Update: {item.updated_at.toString().slice(0,10)}</p>
+        </div>
+      ))}
+    </div>
+    )
 }
 
 export default Portfolio
